6e08999422cc034dd8b55f3f040295435d49b0bc
Kemboiray <kemboiray@gmail.com>
2022-10-24 13:48:58 -0700

    Print lowercase characters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*Print lowercase letters of alphabet*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 97; c < 123; c++)
+	{
+		putchar(c);
+	}
+	putchar(10);
+	return (0);
+}
