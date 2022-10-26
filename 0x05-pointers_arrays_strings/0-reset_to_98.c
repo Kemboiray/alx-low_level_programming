a49f1a7e900545f8df6024dc80e51f0f85a1f63b
Kemboiray <kemboiray@gmail.com>
2022-10-26 04:49:13 -0700

    Add function description

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 /**
- * modif_my_param - set the integer to 98
- * @n: a pointer the integer we want to set to 98
- *
- * Return: nothing
+ * reset_to_98 - takes an int pointer and updates the int value to 98
+ * @n: integer pointer to the variable to be updated
  */
 void reset_to_98(int *n)
 {
