282ec2d306dd81de3c7db11ea88c3c55c8626f2d
Kemboiray <kemboiray@gmail.com>
2022-10-31 09:43:56 -0700

    Return last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+/**
+ * print_last_digit - return value of last digit
+ * @n: int type variable passed from calling function
+ * Return: Last digit of formal parameter
+ */
+int print_last_digit(int n)
+{
+	int r;
+
+	r = _abs(n % 10);
+	_putchar('0' + r);
+	return (r);
+}
