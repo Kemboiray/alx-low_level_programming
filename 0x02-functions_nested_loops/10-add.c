4a0715db683d6ec21b609c115666f3a82d87ad79
Kemboiray <kemboiray@gmail.com>
2022-10-31 13:37:27 -0700

    Add two integers, return result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+/**
+ * add - return the result of adding two integers
+ * @a: integer value to which another integer variable 'b' is added
+ * @b: integer value added to integer variable 'a'
+ * Return: result of addition
+ */
+int add(a, b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
