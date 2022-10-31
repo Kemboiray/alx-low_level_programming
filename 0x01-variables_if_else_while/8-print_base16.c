70a70ef2230a5ca8782b5538cb5a6d608f5e367e
Kemboiray <kemboiray@gmail.com>
2022-10-30 17:45:13 -0700

    Print hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - print all hex numbers and a new line
+ * Return: Always 0 (SUCCESS)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 48;
+	while (c < 103)
+	{
+		if (c < 58)
+		{
+			putchar(c);
+		}
+		else if (c > 96)
+		{
+			putchar(c);
+		}
+		else
+			putchar(0);
+		c++;
+	}
+	putchar(10);
+	return (0);
+}
