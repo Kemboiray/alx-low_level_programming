fef043e0959214fadd81ae5b0b9bc57502459989
Kemboiray <kemboiray@gmail.com>
2022-10-31 06:06:01 -0700

    Print lowercase alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabet 10x, then newline
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	
+	i = 0;
+	while (i < 10)
+	{
+		for (c = 97; c < 123; c++)
+		{
+			_putchar(c);
+		}
+		i++;
+	}
+	_putchar(10);
+}
