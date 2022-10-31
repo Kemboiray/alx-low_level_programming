cb7b4c7612764cda0114f8da496421ce6d711966
Kemboiray <kemboiray@gmail.com>
2022-10-30 19:28:54 -0700

    Revise

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
-void print_alphabet(void);
 /**
  * main - call print_alphabet
  * Return: 0 (success)
  */
 int main(void)
 {
-	print_alphabet(void);
+	print_alphabet();
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,6 +6,8 @@ int _putchar(char c);
  */
 void print_alphabet(void)
 {
+	char c;
+
 	for (c = 97; c < 123; c++)
 	{
 		_putchar(c);
