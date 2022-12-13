2c38954536051db7a4f6681286b53b8eb805ed24
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2022-12-13 09:10:08 +0300

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - Print single digit numbers in ascending order
+* Return: 0 (Success)
+*/
+int main (void)
+{
+ int i;
+
+ for (i = 48; i <= 57; i++)
+ {
+  if (i != 57)
+  {
+   putchar(i);
+   putchar(44);
+   putchar(32);
+  }
+  else
+  {
+   putchar(i);
+  }
+ }
+ return (0);
+}
