6aa93c3892ce4951ea96eb1f2548c6c6e24e12f7
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2022-12-15 11:29:50 +0300

    Create 9-times_table.c
    Created 9-times_table.c, which defines a function that prints the 9
    times-table.
    Added the new function prototype to main.h.

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - print the 9 times-table, from 0
+ * No arguments
+ * No return value
+ */
+void times_table(void)
+{
+	int i, j, ij;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			ij = i * j;
+			if (j != 9)
+			{
+				printf("%2d, ", ij);
+			}
+			else
+			{
+				printf("%2d", ij);
+			}
+		}
+		printf("\n");
+	}
+}
