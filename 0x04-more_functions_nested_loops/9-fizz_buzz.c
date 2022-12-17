533c0d9fd4ba787118fa46259d33f0c5afd9d5eb
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2022-12-17 15:39:43 +0300

    Create 9-fizz_buzz.c
    This program prints numbers 1 to 100, with exceptions:
    	- For multiples of 3 only, the output is "Fizz"
    	- For multiples of 5 only, the output is "Buzz"
    	- For multiles of both 3 and 5, the output is "FizzBuzz"

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print 1 to 100 with exceptions:
+ *      - for multiples of 3 only, print "Fizz"
+ *      - for multiples of 5 only, print "Buzz"
+ *      - for multiples of both 3 and 5, print "FizzBuzz"
+ * 
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (((i % 3) == 0) && ((i % 5) != 0))
+			printf("Fizz ");
+		else if (((i % 3) != 0) && ((i % 5) == 0))
+			printf("Buzz ");
+		else if (((i % 3) == 0) && ((i % 5) == 0))
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
