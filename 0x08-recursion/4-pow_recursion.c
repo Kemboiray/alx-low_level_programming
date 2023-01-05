f2513c18972672a0c7b11a69224861420254fe82
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-01-06 00:27:21 +0300

    Add condition (exp 0)

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,7 +11,22 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	if (y == 2)
-		return (x * x);
+	if (y == 0)
+		return (1);
+	if (y == 1)
+		return (x);
 	return (x * _pow_recursion(x, y - 1));
 }
+
+int main(void)
+{
+	int x, y;
+
+	printf("Enter base:\n");
+	scanf("%d", &x);
+	printf("Enter exponent:\n");
+	scanf("%d", &y);
+	printf("Function returned %d\n", _pow_recursion(x, y));
+	return (0);
+}
+
