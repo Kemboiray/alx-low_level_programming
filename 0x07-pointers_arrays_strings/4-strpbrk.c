60df853ca6e91e3b7f4a0abf6b1a00a14245bb3a
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-01-06 13:36:58 +0300

    Ad null return

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,17 +11,19 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	int i;
+	int j;
 
-	for (i = 0; s[i]; i++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		for (j = 0; accept[j]; j++)
 		{
 			if (accept[j] == s[i])
-				break;
+				return (s + i);
 		}
-		if (accept[j] == s[i])
-			break;
+/*		if (accept[j] == s[i])
+*			break;
+*/
 	}
-	return (s + i);
+	return (NULL);
 }
