54dc5d6b88e7a3fc73e2cc28af1e4fdfe42b314f
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-01-13 11:57:44 +0300

    Complete task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - adds positive numbers
- * @argc: argument count
- * @argv: pointer to argument vector
+ * @ac: argument count
+ * @av: pointer to argument vector
  *
  * Return: 0 (success), 1 (one of the number contains non-digit symbols).
  */
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-	int sum = 0;
+	int i, j, sum = 0;
 
-	if (argc > 1)
+	for (i = 1; i < ac; i++)
 	{
-		for (argc--)
+		for (j = 0; av[i][j]; j++)
 		{
-			if (atoi(*++argv) >= 0 && atoi(*++argv) < 10)
-				sum += atoi(*++argv);
+			if (!(isdigit(av[i][j])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	while (--ac)
+		sum += atoi(av[ac]);
 	printf("%d\n", sum);
 	return (0);
 }
