f17f9a3f75f93c9190077bd6d289d1f91ef00ec5
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-01-15 00:35:05 +0300

    Attept 2, task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,8 +12,13 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p = malloc(nmemb * size);
-
+	void *p;
+	
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(nmemb * size);
 	if (p == NULL)
 	{
 		free(p);
