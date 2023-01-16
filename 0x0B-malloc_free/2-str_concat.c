98aaf5c1e78c43f0a047f4be63e0f0699cb6c354
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-01-16 19:07:29 +0300

    Task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "2-strlen.c"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ *
+ * Return: pointer to memory location containing the concatenation (success),
+ * or NULL (failure)
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i, j;
+	char *p = malloc(_strlen(s1) + _strlen(s2) + 1);
+
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+
+	for (i = 0; s1[i]; i++)
+	{
+		p[i] = s1[i];
+	}
+
+	for (j = 0; s2[j]; i++, j++)
+	{
+		p[i] = s2[j];
+	}
+	p[i] = 0;
+
+	return (p);
+}
