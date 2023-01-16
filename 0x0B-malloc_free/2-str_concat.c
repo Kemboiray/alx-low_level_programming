c3f8f31a44c42e5125e0a84463cac82cf76c8697
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-01-16 23:20:13 +0300

    Revise 1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,4 @@
 #include "2-strlen.c"
-
 /**
  * str_concat - concatenates two strings
  * @s1: pointer to first string
@@ -11,24 +10,54 @@
 char *str_concat(char *s1, char *s2)
 {
 	unsigned int i, j;
-	char *p = malloc(_strlen(s1) + _strlen(s2) + 1);
+	char *p;
 
-	if (p == NULL)
+	if (s1 != NULL && s2 != NULL)
 	{
-		free(p);
+		p = malloc(_strlen(s1) + _strlen(s2) + 1);
+		if (p == NULL)
+		{
+			free(p);
+			return (NULL);
+		}
+		for (i = 0; s1[i]; i++)
+		{
+			p[i] = s1[i];
+		}
+		for (j = 0; s2[j]; i++, j++)
+		{
+			p[i] = s2[j];
+		}
+		p[i] = 0;
+		return (p);
+	}
+	if (s1 == NULL && s2 != NULL)
+	{
+		p = malloc(_strlen(s2) + 1);
+		if (p == NULL)
+		{
+			free(p);
+			return (NULL);
+		}
+		for (i = 0; s2[i]; i++)
+			p[i] = s2[i];
+		p[i] = 0;
+		return (p);
+	}
+	if (s1 != NULL && s2 == NULL)
+	{
+		p = malloc(_strlen(s1) + 1);
+		if (p == NULL)
+		{
+			free(p);
+			return (NULL);
+		}
+		for (i = 0; s1[i]; i++)
+			p[i] = s1[i];
+		p[i] = 0;
+		return (p);
+	}
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-	}
-
-	for (i = 0; s1[i]; i++)
-	{
-		p[i] = s1[i];
-	}
-
-	for (j = 0; s2[j]; i++, j++)
-	{
-		p[i] = s2[j];
-	}
-	p[i] = 0;
-
-	return (p);
+	return (NULL);
 }
