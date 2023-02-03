100e9e175cf0aaac5f604bb9e95e8c1da42cf74a
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-02-03 04:22:46 +0300

    Attempt task 9 again

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,6 @@
 #include "lists.h"
-
+#include "2-add_nodeint.c"
+#include "7-get_nodeint.c"
 /**
  * insert_nodeint_at_index - inserts node in the nth position of a listint_t
  * list.
@@ -9,21 +10,21 @@
  *
  * Return: pointer to inserted node (success) or NULL (failure).
 */
-listint_t *insert_nodeint_at_index(listint_t *head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *node, *newnode;
 
 	if (idx == 0)
 	{
-		newnode = add_nodeint(*head, (const int)n);
+		newnode = add_nodeint(head, (const int)n);
 		return (newnode);
 	}
-	node = get_nodeint_at_index(head, idx - 1);
+	node = get_nodeint_at_index(*head, idx - 1);
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);
 	newnode->n = n;
-	newnode->next = get_nodeint_at_index(head, idx);
+	newnode->next = get_nodeint_at_index(*head, idx);
 	node->next = newnode;
 	return (newnode);
 }
