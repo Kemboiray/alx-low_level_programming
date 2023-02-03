1f73fda80b069a8a6fe8763b61436339a9ff63b7
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-02-03 20:56:23 +0300

    Return NULL if idx exceeds list size

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include "1-listint_len.c"
 #include "2-add_nodeint.c"
 #include "7-get_nodeint.c"
 /**
@@ -6,14 +7,17 @@
  * list.
  * @head: pointer to the first node.
  * @idx: integer value corresponding to the position of nth node.
- * @n: integer valuewith which to initialise the new node.
+ * @n: integer value with which to initialise the new node.
  *
  * Return: pointer to inserted node (success) or NULL (failure).
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *node, *newnode;
+	size_t size = listint_len((const listint_t *)(*head));
 
+	if (idx >= size)
+		return (NULL);
 	if (idx == 0)
 	{
 		newnode = add_nodeint(head, (const int)n);
