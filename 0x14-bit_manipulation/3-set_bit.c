362d54b0a1c4b4d4f84e30dc2067f6cb095d021c
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-02-07 19:53:33 +0300

    Attempt task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "0-binary_to_uint.c"
 
 /**
  * set_bit -  sets the value of a bit to 1 at a given index.
@@ -9,5 +10,20 @@
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	;
+	char *arr;
+	unsigned int i, num;
+
+	arr = malloc((index + 2) * sizeof(*arr));
+	if (arr == NULL)
+		return (-1);
+
+	arr[0] = '1';
+	for (i = 1; i < index + 1; i++)
+		arr[i] = '0';
+	arr[index + 1] = 0;
+
+	num = binary_to_uint(arr);
+	*n |= num;
+
+	return (1);
 }
