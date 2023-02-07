b2dbaf1dc792ae17b6f3a9d3cfa7ad4a5425e831
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-02-07 13:46:52 +0300

    Use recursion

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,25 +8,15 @@
  */
 void print_binary(unsigned long int n)
 {
-	int size = 0, tmp1 = n, tmp2, i, arr[20];
+	unsigned long int num;
 
-	if (n == 0)
+	if (n < 2)
 	{
-		printf("%d", 0);
+		_putchar(n + 48);
 		return;
 	}
-	while (n > 0)
-	{
-		n >>= 1;
-		size++;
-	}
-	tmp2 = size;
-	while (size > 0)
-	{
-		arr[size - 1] = tmp1 & 1;
-		tmp1 >>= 1;
-		size--;
-	}
-	for (i = 0; i < tmp2; i++)
-		printf("%d", arr[i]);
+	num = n & 1;
+	n >>= 1;
+	print_binary(n);
+	_putchar(num + 48);
 }
