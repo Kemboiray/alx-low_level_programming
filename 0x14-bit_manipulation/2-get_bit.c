416cd24cb1fd8e22e2cfecf7b93e39a6d5cf9354
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-02-07 17:23:18 +0300

    Attempt task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,5 +9,47 @@
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	;
+	unsigned long int tmp1 = n;
+	int size, tmp2, ret, *arr;
+
+	size = bit_count(n);
+
+	if (index >= (unsigned int)size)
+		return (-1);
+
+	arr = malloc(size * sizeof(*arr));
+	if (arr == NULL)
+		return (-1);
+
+	tmp2 = size;
+	while (tmp2 > 0)
+	{
+		arr[tmp2 - 1] = tmp1 & 1;
+		tmp1 >>= 1;
+		tmp2--;
+	}
+
+	ret = arr[size - 1 - index];
+	free(arr);
+	return (ret);
+}
+
+/**
+ * bit_count - count the number of bits in a number.
+ * @n: number.
+ *
+ * Return: bit count.
+*/
+int bit_count(unsigned long int n)
+{
+	int size;
+
+	if (n == 0 || n == 1)
+		return (1);
+	while (n > 0)
+	{
+		n >>= 1;
+		size++;
+	}
+	return (size);
 }
