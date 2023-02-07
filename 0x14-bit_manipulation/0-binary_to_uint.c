e7f5877789d30bfae8688d151b02f67200d2aaec
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-02-07 08:29:55 +0300

    Attempt task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,9 +5,29 @@
  * @b: pointer to string of 0 and 1 chars.
  *
  * Return: converted number or 0 (one or more characters in source
- * string != '1' | '0', or b == NULL)
+ * string != '1' | '0', or b == NULL).
 */
 unsigned int binary_to_uint(const char *b)
 {
-	;
+	unsigned int *mul, i, j, len, _mul = 1, sum = 0;
+
+	if (b == NULL)
+		return (0);
+	for (len = 0; b[len]; len++)
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+	mul = malloc(len * sizeof(len));
+	if (mul == NULL)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		for (j = i + 1; b[j]; j++)
+			_mul *= 2;
+		mul[i] = _mul;
+		_mul = 1;
+		sum += ((unsigned int) (b[i] - 48)) * mul[i];
+	}
+
+	return (sum);
 }
