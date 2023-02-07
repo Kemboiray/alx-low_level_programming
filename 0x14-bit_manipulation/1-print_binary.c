df1ff028031d3305d18d4dba420f066540d64923
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-02-07 09:13:18 +0300

    Attempt task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,5 +8,29 @@
  */
 void print_binary(unsigned long int n)
 {
-	;
+	int size = 0, tmp1 = n, tmp2, i, *arr;
+
+	if (n == 0)
+	{
+		printf("%d", 0);
+		return;
+	}
+	while (n > 0)
+	{
+		n >>= 1;
+		size++;
+	}
+	arr = malloc(size * sizeof(int));
+	if (arr == NULL)
+		exit(EXIT_FAILURE);
+	tmp2 = size;
+	while (size > 0)
+	{
+		arr[size - 1] = tmp1 & 1;
+		tmp1 >>= 1;
+		size--;
+	}
+	for (i = 0; i < tmp2; i++)
+		printf("%d", arr[i]);
+	free(arr);
 }
