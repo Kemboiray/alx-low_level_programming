38c78c7282bc8e6e7e008947b3fedcb794d3cb8c
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-02-10 13:52:40 +0300

    Revise task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,47 @@
 #include "main.h"
 
 /**
- * read_textfile - function
- * @filename: filename
- * @letters: letters
+ * read_textfile - reads a text file and prints it to the POSIX standard
+ * output
+ * @filename: pointer to string describing the text file name
+ * @letters: number of letters to print
  *
- * Return: bytes written
+ * Return: bytes read and printed (success), or 0 (filename is NULL, open,
+ * read or write failed).
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t wrt;
-	int opn, cls;
+	ssize_t wrt, rd;
+	char *buf;
+	int opn;
 
 	if (filename == NULL)
 		return (0);
 
-	opn = open(filename, O_RDWR);
+	opn = open(filename, O_RDONLY);
 	if (opn < 0)
 		return (0);
 
-/*	printf("opn = %d\n", opn);*/
-	wrt = write(STDOUT_FILENO, filename, letters);
-	if (wrt < (signed int)letters)
+	buf = malloc(letters * sizeof(*buf));
+	if (buf == NULL)
+	{
+		close(opn);
+		return (0);
+	}
+
+	rd = read(opn, buf, letters);
+	if (rd < 0)
 		return (0);
 
-	cls = close(opn);
-	if (cls != 0)
+	wrt = write(STDOUT_FILENO, buf, (size_t)rd);
+	if (wrt < rd)
+	{
+		close(opn);
+		free(buf);
 		return (0);
+	}
 
+	close(opn);
+	free(buf);
 	return (wrt);
 }
