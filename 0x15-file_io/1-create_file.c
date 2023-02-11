fd551e9f6c69e289db083873d0c63b64fbf763fa
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-02-11 12:41:51 +0300

    Debug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,11 @@ int create_file(const char *filename, char *text_content)
 
 	wrt = write(fd, text_content, strlen(text_content) + 1);
 	if (wrt == -1)
+	{
+		close(fd);
 		return (-1);
+	}
 
+	close(fd);
 	return (1);
 }
