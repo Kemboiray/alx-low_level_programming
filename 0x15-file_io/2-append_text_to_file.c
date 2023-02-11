4a39097c5cd2253567f04e234e460c98efe96209
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-02-11 12:25:56 +0300

    Attempt task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: pointer to string describing file name
+ * @text_content: pointer to buffer with NULL termiated text to append
+ *
+ * Return: 1 (success), or -1 (failure - filename == NULL, file does not exist,
+ * or write fails).
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t wrt;
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_APPEND | O_WRONLY);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
+	wrt = write(fd, text_content, strlen(text_content) + 1);
+	if (wrt == -1)
+		return (-1);
+
+	return (1);
+}
