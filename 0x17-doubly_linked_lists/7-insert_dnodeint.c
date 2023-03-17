7c49ece5d939d5ad2402438d5124d84f441e5eec
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-17 14:36:35 +0300

    Complete insertion of new node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,6 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new_node->next = next_node;
 		new_node->prev = next_node->prev;
 		next_node->prev = new_node;
+		new_node->prev->next = new_node;
 	}
 
 	return (new_node);
