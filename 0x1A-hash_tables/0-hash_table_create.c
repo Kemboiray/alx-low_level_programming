69b7141eeddf5a58ab8c7f591852eddd374af1da
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-04-07 21:01:47 +0300

    Allocate memory for ``hash_node_t *``, not ``hash_node_t``

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,7 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 /*	unsigned long int i;*/
 
 	table.size = size;
-	table.array = malloc(sizeof(hash_node_t) * size);
+	table.array = malloc(sizeof(hash_node_t *) * size);
 
 	if (table.array == NULL)
 		return (NULL);
