8a9651d0c605e2146a8189aae406fe822bb1e607
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-04-08 00:21:00 +0300

    Create 3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: pointer to hash table
+ * @key: key mapping to value
+ * @value: value
+ *
+ * Return: 1 (success) or 0 (failure.)
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node;
+	unsigned long int index;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = (char *)key;
+	new_node->value = (char *)value;
+	new_node->next = (ht->array)[index];
+	(ht->array)[index] = new_node;
+
+	return (1);
+}
