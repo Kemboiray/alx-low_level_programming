dcb567b699acc9c34c09148457d940d0a5b422a0
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-04-10 15:33:49 +0300

    Correct syntax in comparison

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,13 +13,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *temp = NULL;
 
-	if (ht == NULL)
+	if (ht == NULL || key == NULL)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	temp = (ht->array)[index];
 	while (temp)
 	{
-		if ((char *)key == temp->key)
+		if (strcmp(temp->key, key) == 0)
 			return (temp->value);
 		temp = temp->next;
 	}
