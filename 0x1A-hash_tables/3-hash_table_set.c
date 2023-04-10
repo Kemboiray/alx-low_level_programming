eea885381ed8ce06638a7ad46e9f02232545dd0f
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-04-10 15:22:31 +0300

    Handle collisions

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,17 +12,28 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node;
+	hash_node_t *new_node = NULL, *temp_node = NULL;
 	unsigned long int index;
 
 	if (ht == NULL || key == NULL || value == NULL || key[0] == 0)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+
+	temp_node = (ht->array)[index];
+	if (temp_node)
+	{
+		if (strcmp(temp_node->key, key) == 0)
+		{
+			free(temp_node->value);
+			temp_node->value = strdup(value);
+			return (1);
+		}
+	}
+
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
