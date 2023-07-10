e6900db04fb662054eb0ab6300add8c0211e32a8
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-07-10 18:30:19 +0300

    Task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+ * linear_search - search for a value in an array of integer
+ * @array: pointer to array of integers
+ * @size: size of @array
+ * @value: value to search for
+ *
+ * Return: first matching index, or -1 (@array is NULL or match not found)
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array)
+		for (i = 0; i < size; i++)
+		{
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+			{
+				printf("Found %d at index: %lu\n", value, i);
+				return (i);
+			}
+		}
+
+	return (-1);
+}
