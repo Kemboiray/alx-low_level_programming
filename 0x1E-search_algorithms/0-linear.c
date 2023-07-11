61b3bd40b1b8b5bf4e84ae69c33d4a81ae9e4131
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-07-11 13:37:33 +0300

    gitignore binary

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,4 @@
 #include "search_algos.h"
-#include <stdio.h>
 
 /**
  * linear_search - search for a value in an array of integer
@@ -18,10 +17,7 @@ int linear_search(int *array, size_t size, int value)
 		{
 			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 			if (array[i] == value)
-			{
-				printf("Found %d at index: %lu\n", value, i);
 				return (i);
-			}
 		}
 
 	return (-1);
