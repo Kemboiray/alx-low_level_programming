6224c9d4ab37fc50ae7507ff898bac4d9100e0ce
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-07-12 17:17:20 +0300

    Task 8

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,59 +2,44 @@
 #include <math.h>
 
 /**
- * linear_search - search for a value in an array of integers
- * @array: pointer to array of integers
- * @size: size of @array
- * @value: value to search for
- *
- * Return: first matching index, or -1 (@array is NULL or match not found)
- */
-int linear_search(int *array, size_t size, int value)
-{
-	size_t i;
-
-	if (array)
-		for (i = 0; i < size; i++)
-		{
-			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-			if (array[i] == value)
-				return (i);
-		}
-
-	return (-1);
-}
-
-/**
  * jump_search - search for a value in an array of integers
  * @array: pointer to array of integers
  * @size: size of @array
  * @value: value to search for
  *
- * Return: first matching index, or -1 (@array is NULL or match not found)
+ * Return: first matching index, or -1 (@array is NULL or no match found)
  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t i, jump;
+	size_t prev = 0, root, step;
 
-	if (array)
+	if (array == NULL || size == 0)
+		return (-1);
+	root = (size_t)floor(sqrt(size));
+	step = root;
+
+	while (array[(step < size ? step : size) - 1] < value)
 	{
-		jump = (size_t)(round(sqrt(size)));
-
-		for (i = 0; i < size; i += jump)
-		{
-			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-			if (array[i] == value)
-			{
-				return (i);
-			}
-
-			if (array[i + 1] > value)
-			{
-				printf("Value found between indexes [%lu] and [%lu]\n", i - jump, i);
-				return (linear_search(&array[i - jump], jump, value) + i - jump);
-			}
-		}
+		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+		prev = step;
+		step += root;
+		if (prev >= size)
+			return (-1);
+	}
+/*
+*	printf("Value found between indexes [%lu] and [%lu]\n",
+*			prev - root, step - root);
+*/
+	while (array[prev] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+		prev++;
+		if (prev == (step < size ? step : size))
+			return (-1);
 	}
 
+	printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+	if (array[prev] == value)
+		return (prev);
 	return (-1);
 }
